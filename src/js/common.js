2e02f7b4289a1a40e3cc89cfd62768ab4c99118d
Administrator <simple852@naver.com>
2024-06-17 16:03:40 +0900

    반응형 디스플레이 보완

--- src/js/common.js
+++ src/js/common.js
@@ -1,7 +1,8 @@
 const body = document.body;
 const titleInput = document.getElementById('titleInput');
-const menuIcon = document.getElementById('menuIcon');
 const dialog = document.getElementById('dialog');
+const menuCloseIcon = document.getElementById('menuCloseIcon');
+const menuCloseIconDarkMode = document.getElementById('menuCloseIconDarkMode');
 
 // 스크롤 맨 위로 이동 버튼 함수
 const handleScrollUpBtn = () => {
@@ -16,12 +17,26 @@ const handleDarkModeToggle = () => {
     const body = document.querySelector('body');
     body.classList.toggle('dark-mode');
     const isDarkMode = body.classList.contains('dark-mode');
+
+    if (isDarkMode) {
+        menuCloseIcon.style.display = 'none';
+        menuCloseIconDarkMode.style.display = 'block';
+    } else {
+        menuCloseIcon.style.display = 'block';
+        menuCloseIconDarkMode.style.display = 'none';
+    }
+
     localStorage.setItem('dark-mode', isDarkMode);
 }
 
 // 만약 이전 페이지에서 다크모드를 활성화 했으면 다음 페이지에서도 활성화 유지
 if (localStorage.getItem('dark-mode') === 'true') {
     body.classList.add('dark-mode');
+    menuCloseIcon.style.display = 'none';
+    menuCloseIconDarkMode.style.display = 'block';
+} else {
+    menuCloseIcon.style.display = 'block';
+    menuCloseIconDarkMode.style.display = 'none';
 }
 
 let value;
@@ -33,12 +48,17 @@ const handleSearch = () => {
     window.location.href = `search.html?title=${value}`;
 }
 
-menuIcon.addEventListener('click', function () {
+const handleMenuIcon = () => {
     dialog.classList.add('visible');
-})
+}
 
 dialog.addEventListener('click', function (e) {
     if (e.target === dialog) {
         dialog.classList.remove('visible');
     }
 })
+
+const handleMenuCloseIcon = () => {
+    dialog.classList.remove('visible');
+}
+
